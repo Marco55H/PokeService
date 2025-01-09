9c2f5d02be8dff56d82e0266f371d69016ac600b
Marcos <marco.holguin@iesnervion.es>
2025-01-09 22:47:04 +0100

    yy

--- PokeService/ViewModels/PokemonsVM.cs
+++ PokeService/ViewModels/PokemonsVM.cs
@@ -3,29 +3,43 @@ using PokeService.ViewModels.Utility;
 using Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PokeService.ViewModels
 {
-    internal class PokemonsVM
+    internal class PokemonsVM: INotifyPropertyChanged
     {
         #region Atributos
-        private List<ClsPokemon> pokemons;
+        private ObservableCollection<ClsPokemon> pokemons;
         private DelegateCommand cmdPedirPokemons;
         private int cont=0;
+        private int id = 0;
         #endregion
 
         #region Propiedades
-        public List<ClsPokemon> Pokemons {  get { return pokemons; } }
+        public ObservableCollection<ClsPokemon> Pokemons 
+        {  
+            get
+            {
+                return pokemons;
+            }
+            set
+            {
+                NotifyPropertyChanged("pokemons"); pokemons = value;
+            }
+        }
         public DelegateCommand CmdPedirPokemons { get {return cmdPedirPokemons;} }
         #endregion
 
         #region Constructores
         public PokemonsVM() 
         { 
-            pokemons = new List<ClsPokemon>();
+            pokemons = new ObservableCollection<ClsPokemon>();
             cmdPedirPokemons = new DelegateCommand(cmdPedirPokemons_Execute, cmdPedirPokemons_CanExecute);
         }
 
@@ -42,10 +56,24 @@ namespace PokeService.ViewModels
             ClsManejadora manejadora = new ClsManejadora();
             List<ClsPokemon> listaPokemon = await manejadora.getPokemons(cont);
 
-            pokemons.AddRange(listaPokemon);
-            cont += 20;
-            
+
+            foreach (ClsPokemon pokemon in listaPokemon)
+            {
+                pokemons.Add(pokemon);
+            }
+
+            cont += 20;           
         }
         #endregion
+
+        #region Property Changed
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
     }
 }
